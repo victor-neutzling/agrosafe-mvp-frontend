2ba16653373aa40d3c820d4e3323b82adc8bfddd
Sarah <email.sarahabreu@gmail.com>
2026-04-18 10:30:50 -0300

    feat: tela de login e tela de cadastro

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,15 @@
-import { createBrowserRouter } from "react-router";
-import Placeholder from "./pages/placeholder";
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/cadastro";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Placeholder />,
+    element: <Login />,
+  },
+  {
+    path: "/cadastro", 
+    element: <Register />,
   },
 ]);
 
